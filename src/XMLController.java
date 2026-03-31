d1a9df472871b90646056d3257bfe1aa4bdebb74
mhmdoch <patrick.wuppertal@gmail.com>
2026-03-31 18:48:34 +0200

    keeping try {} small

--- src/XMLController.java
+++ src/XMLController.java
@@ -13,32 +13,20 @@ public class XMLController {
 
 
     public void readXML(File file) {
-        int scoreValue = 0;
         Document doc = null;
 
         try {
             DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             doc = builder.parse(file);
             doc.getDocumentElement().normalize();
-
-
-            NodeList nodeList = doc.getElementsByTagName("totalScore");
-            Node first = nodeList.item(0);
-            scoreValue = Integer.valueOf(first.getTextContent());
-
         } catch (Exception e) {
 
         }
+        NodeList nodeList = doc.getElementsByTagName("totalScore");
+        Node first = nodeList.item(0);
+        int scoreValue = Integer.valueOf(first.getTextContent());
         System.out.println(scoreValue);
-        // das hier liefert [#document: null]
-        // obwohl ich hier die gesamte xml.normalize() erwartet habe
-        // aber das erwartet habe ich auch irgendwie bekommen
-        // nur liegen die daten nicht mehr so xml artig im speicher
-        // sondern als verschachtelte baumstruktur
-        // doc = builder.parse(file) macht das
 
-
-        System.out.println(doc);
 
     }
 
