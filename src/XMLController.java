333aafbdc5809357ae864aaad08108c635e99712
mhmdoch <patrick.wuppertal@gmail.com>
2026-03-31 20:50:42 +0200

    playerPos, playerPosY, playerName, playerSpeed, playerHealth, playerBaseDmg are read

--- src/XMLController.java
+++ src/XMLController.java
@@ -1,12 +1,12 @@
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.util.ArrayList;
 
 
 public class XMLController {
@@ -33,27 +33,41 @@ public class XMLController {
         // int totalScore
         NodeList totalScoreNodeList = doc.getElementsByTagName("totalScore");
         Node totalScoreNode = totalScoreNodeList.item(0);
-        int totalScore = Integer.valueOf(totalScoreNode.getTextContent());
-/*
+        //int totalScore = Integer.valueOf(totalScoreNode.getTextContent());
+
+        int totalScore = Integer.parseInt(doc.getElementsByTagName("totalScore").item(0).getTextContent());
+
+
         // Player player
         NodeList playerNodeList = doc.getElementsByTagName("player");
+        // playerNodeList wird durch (Element) in playerElement zu einem Element
+        Element playerElement = (Element) playerNodeList.item(0);
 
-        int playerPosX = Integer.parseInt(playerNodeList.getAttribute("posX"));
-        int playerPosY = Integer.parseInt(playerNodeList.getAttribute("posY"));
+        // int playerPosX, int playerPosY, String playerName
+        int playerPosX = Integer.parseInt(playerElement.getAttribute("posX"));
+        int playerPosY = Integer.parseInt(playerElement.getAttribute("posY"));
+        String playerName = playerElement.getElementsByTagName("name").item(0).getTextContent();
+        int playerSpeed = Integer.parseInt(playerElement.getElementsByTagName("speed").item(0).getTextContent());
+        int playerHealth = Integer.parseInt(playerElement.getElementsByTagName("health").item(0).getTextContent());
+        int playerBaseDmg = Integer.parseInt(playerElement.getElementsByTagName("baseDmg").item(0).getTextContent());
 
-/*
-    <player posX="16" posY="8">
-                <name>Udo</name>
-                <speed>30</speed>
-                <health>100</health>
-                <baseDmg>500</baseDmg>
-                </player>
-*/
+
+
 
         System.out.println("mapWidth: " + mapWidth);
         System.out.println("mapHeight: " + mapHeight);
-
         System.out.println("totalScore: " + totalScore);
+        System.out.println("------------------------------");
+
+        System.out.println("playerPosX: " + playerPosX);
+        System.out.println("playerPosY: " + playerPosY);
+        System.out.println("Name: " + playerName);
+        System.out.println("speed: " + playerSpeed);
+        System.out.println("health: " + playerHealth);
+        System.out.println("baseDmg: " + playerBaseDmg);
+        System.out.println("------------------------------");
+
+
 
 
     }
